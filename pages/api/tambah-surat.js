142f126aab50c1301603d7853d7b8e206f074468
odoy89 <asep.saif@gmail.com>
2026-01-17 19:37:20 +0700

    Create tambah-surat.js

---
+++ pages/api/tambah-surat.js
@@ -0,0 +1,42 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      status: "error",
+      message: "Method not allowed"
+    });
+  }
+
+  try {
+    // pastikan body object
+    const payload =
+      typeof req.body === "string"
+        ? JSON.parse(req.body)
+        : req.body;
+
+    if (!payload?.action) {
+      return res.status(400).json({
+        status: "error",
+        message: "Action tidak ditemukan"
+      });
+    }
+
+    const response = await fetch(
+      process.env.NEXT_PUBLIC_APPSCRIPT_URL,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      }
+    );
+
+    const result = await response.json();
+    return res.status(200).json(result);
+
+  } catch (err) {
+    console.error("API TAMBAH SURAT ERROR:", err);
+    return res.status(500).json({
+      status: "error",
+      message: "Gagal koneksi ke AppScript"
+    });
+  }
+}
