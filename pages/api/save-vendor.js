fed62fa3d06b91bfa88332d5092027f61d1216d9
odoy89 <asep.saif@gmail.com>
2026-01-17 20:40:41 +0700

    Update save-vendor.js

--- pages/api/save-vendor.js
+++ pages/api/save-vendor.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { NO, VENDOR, KONTAK_VENDOR } = req.body;
 
-  const r = await fetch(process.env.APPSCRIPT_URL, {
+  const r = await fetch(process.env.NEXT_PUBLIC_APPSCRIPT_URL, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
@@ -15,3 +15,4 @@ export default async function handler(req, res) {
   const data = await r.json();
   res.status(200).json(data);
 }
+
